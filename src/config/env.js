320483b106ca078031385f368aacb79b6ed2daa6
Carlos Manuel Vargas <cmv-66@hotmail.com>
2026-06-19 19:55:08 -0300

    feat: add mongoDB connect

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,5 @@ dotenv.config();
 
 export const env = {
     PORT: process.env.APP_PORT || 3000,
+    MONGO_URI: process.env.MONGODB_URI
 }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 import { env } from "./config/env.js";
 
 const PORT = env.PORT;
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
